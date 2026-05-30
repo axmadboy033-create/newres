1e1303cf6a1d32c2f715f37f948a41b9470695af
axmadboy033-create <axmadboy033@gmail.com>
2026-05-30 15:29:41 +0500

    create files

---
+++ src/components/Axmadxon.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Axmadxon = () => {
+  return (
+    <div>Axmadxon</div>
+  )
+}
+
+export default Axmadxon
---
+++ src/components/Ozodbek.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Ozodbek = () => {
+  return (
+    <div>Ozodbek</div>
+  )
+}
+
+export default Ozodbek
