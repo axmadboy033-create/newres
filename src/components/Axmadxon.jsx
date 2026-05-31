3317d45984965af6893d78996c06b8783c86a51b
axmadboy033-create <axmadboy033@gmail.com>
2026-05-31 11:15:47 +0500

    fudbolka

--- src/components/Axmadxon.jsx
+++ src/components/Axmadxon.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
+import f1 from ""
 
 const Axmadxon = () => {
   return (
     <div>
-        <h1>Axmadxon</h1>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Error, ut!</p>
+       <div className="container">
+         <div className="ahref">
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Shop</a></li>
+                <li><a href="#">Men</a></li>
+                <li>T-shirts</li>
+                <div className="fudbolka">
+                    <img src="" alt="" />
+                </div>
+            </ul>
+         </div>
+       </div>
     </div>
   )
 }
